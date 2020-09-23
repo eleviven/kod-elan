482f086d4c830c9df9a9c9b8d8710af38dd0e16d
eleviven <haciyev@Firuz-MacBook-Pro.local>
2020-09-23 05:41:47 +0400

    Next init

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: {
+    "postcss-custom-media": {
+      importFrom: "./public/css/viewport.css",
+    },
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+        "nesting-rules": true,
+      },
+    },
+  },
+};
