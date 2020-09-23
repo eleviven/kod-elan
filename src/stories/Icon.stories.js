7636a279714d31dd2438fa40e02c879ade0455e1
eleviven <haciyev@Firuz-MacBook-Pro.local>
2020-09-24 01:25:52 +0400

    Icon stories created

---
+++ src/stories/Icon.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import * as Icon from "../components/icons";
+
+export default {
+  title: "Icons",
+};
+
+export const AllIcons = () => (
+  <div class="item-grid">
+    <Icon.Plus />
+    <Icon.Minus />
+    <Icon.Options />
+    <Icon.Close />
+    <Icon.ArrowLeft />
+    <Icon.ArrowRight />
+    <Icon.Eye />
+    <Icon.Grid />
+    <Icon.List />
+    <Icon.Megaphone />
+    <Icon.Search />
+  </div>
+);
